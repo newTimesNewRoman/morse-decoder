61453c68458f00f6325294c17d59efd01926465d
Roman S <roman.sukhopleshchenko@yandex.ru>
2022-07-30 18:22:36 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let x = expr.length % 10;
+    let prepareExpr = (x !== 0) ? '0'.repeat(10 - x) + expr : expr;
+    let prepareArrayExpr = prepareExpr.match(/.{1,10}/g);
+    let resultArrayExpr = [];
+    for (let elem of prepareArrayExpr) {
+        if (elem === '**********') {
+            resultArrayExpr.push(' ');
+        } else { 
+            let elementDecode = '';
+            for (let i = 0; i < elem.length; i += 2) {
+                if ((elem[i] + elem[i + 1]) === '00') {elementDecode = elementDecode + ''};
+                if ((elem[i] + elem[i + 1]) === '10') {elementDecode = elementDecode + '.'};
+                if ((elem[i] + elem[i + 1]) === '11') {elementDecode = elementDecode + '-'};
+            }
+            resultArrayExpr.push(elementDecode);
+        }
+    }; 
+    let result = resultArrayExpr.map((symbol) => {
+        (symbol === ' ') ?  symbol = symbol : symbol = MORSE_TABLE[symbol];
+        return symbol;
+    });
+    return result.join('');
 }
 
 module.exports = {
